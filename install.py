b4cc7bada5a12f056e294a018751faeaf669c46f
Ramanan Sivaranjan <ramanan@funkaoshi.com>
2010-11-22 06:43:23 -0800

    Install script actually works.

--- install.py
+++ install.py
@@ -3,9 +3,12 @@ import os
 exclude = ['README', 'install.py']
 
 home = os.path.expanduser('~')
+here = os.path.abspath('.')
 for f in os.listdir('.'):
     if f in exclude or f[0] == '.':
         continue
-    cmd = 'ln -s %s %s/.%s' % (f, home, f)
+    local_file = "%s/%s" % (here, f)
+    install_file = "%s/.%s" % (home, f)
+    cmd = 'ln -sf %s %s' % (local_file, install_file)
     print cmd
     os.system(cmd)
